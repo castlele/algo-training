69a387d8b555dd2ff832bee82333642a7e92801a
Nikita Semenov <castlelecs@gmail.com>
2024-01-10 08:00:14 +0300

    kata: repeate of longest substring without repeating characters leetcode in kotlin

--- kata/kotlinKatas/lib/src/main/kotlin/kotlinkatas/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersSolution.kt
+++ kata/kotlinKatas/lib/src/main/kotlin/kotlinkatas/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharactersSolution.kt
@@ -2,26 +2,27 @@ package longestsubstringwithoutrepeatingcharacters
 
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        var length = 0
-
         val indexMap = mutableMapOf<Char, Int>()
-        var right = 0
+
+        var len = 0
         var left = 0
+        var right = 0
 
         while (right < s.count()) {
-            val currentChar = s.get(right)
+            s.getOrNull(right)
+                ?.let { currentChar ->
+                    indexMap.getOrElse(currentChar) { null }
+                        ?.let {
+                            left = Math.max(it + 1, left)
+                        }
 
-            indexMap
-                .get(currentChar)
-                ?.let {
-                    left = Math.max(it + 1, left)
+                    indexMap.put(currentChar, right)
                 }
 
-                indexMap.put(currentChar, right)
-                length = Math.max(right - left + 1, length)
-                right++
+            len = Math.max(right - left + 1, len)
+            right++
         }
 
-        return length
+        return len
     }
 }
